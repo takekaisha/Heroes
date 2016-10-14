0ccb794e66b4ddcc113b7cfba941a2d0ff8f5349
takekaisha <take_kaisha@yahoo.co.jp>
2016-10-14 00:56:41 +0000

    first commit

---
+++ angular-quickstart/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Promise<Hero[]>{
+        return Promise.resolve(HEROES);        
+    }
+    
+    getHeroesSlowly(): Promise<Hero[]>{
+        return new Promise<Hero[]>(resolve =>
+        setTimeout(resolve, 2000))
+        .then(() => this.getHeroes());
+    }
+}
