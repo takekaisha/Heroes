cba9aa3b0687c326573cbf23178a712ea0d91978
takekaisha <take_kaisha@yahoo.co.jp>
2016-10-19 08:27:25 +0000

    Add-Http

---
+++ angular-quickstart/app/in-memory-data.service.js
@@ -0,0 +1,24 @@
+"use strict";
+var InMemoryDataService = (function () {
+    function InMemoryDataService() {
+    }
+    InMemoryDataService.prototype.createDb = function () {
+        var heroes = [
+            { id: 11, name: 'Mr Naice' },
+            { id: 12, name: 'Narco' },
+            { id: 13, name: 'Bombasto' },
+            { id: 14, name: 'Celeritas' },
+            { id: 15, name: 'Magneta' },
+            { id: 16, name: 'RubberMan' },
+            { id: 17, name: 'Dynama' },
+            { id: 18, name: 'Dr IQ' },
+            { id: 19, name: 'Magma' },
+            { id: 20, name: 'Tornado' },
+            { id: 21, name: 'Take' }
+        ];
+        return { heroes: heroes };
+    };
+    return InMemoryDataService;
+}());
+exports.InMemoryDataService = InMemoryDataService;
+//# sourceMappingURL=in-memory-data.service.js.map
